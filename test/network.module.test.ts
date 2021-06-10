a19901a02b32b9768c317f007c8828da77d0ddb0
Elestrias <rus8-2002@mail.ru>
2021-06-10 13:56:01 +0300

    ending of test were added + test logs

--- test/network.module.test.ts
+++ test/network.module.test.ts
@@ -1,28 +1,35 @@
-import { suite, test } from '@testdeck/mocha';
+import { suite, test,  } from '@testdeck/mocha';
 import * as _chai from 'chai';
-import { expect } from 'chai';
+import { expect, assert} from 'chai';
 import {ApiPromise, WsProvider}  from '@polkadot/api'
 import * as fs from  'fs'
 
-_chai.should()
-_chai.expect
 
-@suite class testModule{
-    private WsProvider = new WsProvider('wss://rpc.polkadot.io');
+_chai.should();
+_chai.expect;
+
+@suite class runTimeVersionCheck{
     private api: ApiPromise;
 
-    async before(){
-      this.api  = await ApiPromise.create({provider: this.WsProvider});
+   async after(){
+       console.log("Wainting for a disconection");
+       await this.api.disconnect().then(()=> console.log("-Done"));
     }
-    @test async "versionChecks"(){
-        const localVersion = JSON.parse(fs.readFileSync("./test/local.property.json", "utf-8"));
-        let checkEvent = this.api.query.system.lastRuntimeUpgrade().then(function (response){
-            var nodeProperty = JSON.parse(JSON.stringify(response));
+    @test async "Test1"(){
+        const wsProvider = new WsProvider('wss://rpc.polkadot.io')
+        this.api =  new ApiPromise({provider: wsProvider});
+        var local  = fs.readFileSync("./test/local.property.json", "utf-8");
+        var localVersion = JSON.parse(local);
+        console.log("Waiting for a connection");
+        await this.api.isReady.then(()=>console.log("-Done"));
+        let x = this.api.query.system.lastRuntimeUpgrade().then(function(response) {
+            var property = JSON.parse(JSON.stringify(response));
+            assert(property["specVersion"] === localVersion["specVersion"], "Runtime version: " + property["specVersion"]+ " should be equal to local version: " + localVersion["specVersion"]);
+        });
+        console.log("Waiting for a server response");
+        await x.then(()=>console.log("-Done"));
+   }
 
-            expect(nodeProperty["specName"]).to.be.equal(localVersion["specName"]); // проверил потому что вдруг по каким-либо причинам серевер ответит инфой о другом рантайме и это неправильно будет
-            expect(nodeProperty["specVersion"]).to.be.equal(localVersion["specVersion"]);
 
-        }).catch(console.error);
-        await this.api.disconnect();
-    }
+
 }
