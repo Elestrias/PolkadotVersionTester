08b3c9a63fe2af6ccd515ae24f924abf3aa7bb42
Elestrias <rus8-2002@mail.ru>
2021-06-10 16:25:38 +0300

    bug fixed

--- test/network.module.test.ts
+++ test/network.module.test.ts
@@ -1,23 +1,21 @@
-import { suite, test,  } from '@testdeck/mocha';
-import * as _chai from 'chai';
-import { expect, assert} from 'chai';
+import {suite, test} from '@testdeck/mocha';
+import {assert} from 'chai';
 import {ApiPromise, WsProvider}  from '@polkadot/api'
 import * as fs from  'fs'
 
 
-_chai.should();
-_chai.expect;
 
 @suite class runTimeVersionCheck{
     private api: ApiPromise;
+    private wsProvider = new WsProvider('wss://rpc.polkadot.io');
 
-   async after(){
+
+    async after(){
        console.log("Waiting for an api disconnection");
-       await this.api.disconnect().then(()=> console.log("-Done"));
+       process.exit();
     }
     @test async "Test1"(){
-        const wsProvider = new WsProvider('wss://rpc.polkadot.io')
-        this.api =  new ApiPromise({provider: wsProvider});
+        this.api =  new ApiPromise({provider: this.wsProvider});
         var local  = fs.readFileSync("./test/local.property.json", "utf-8");
         var localVersion = JSON.parse(local);
         console.log("Waiting for an api connection");
